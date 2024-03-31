cdc1cd3d25bc46b8509df95897677e0b49f19f06
KaniSama <naisyboy@gmail.com>
2024-03-31 23:37:37 +0500

    Resource gathering state

--- Scripts/Entities/Spider/dish.gd
+++ Scripts/Entities/Spider/dish.gd
@@ -1,15 +1,35 @@
 extends Node3D
 
+signal editing_mode_entered
 
-@onready var population : int
+
 @onready var modules : Array [ Node3D ]
 @onready var resources : Dictionary = {
+	"population" = 1,
 	"stone" = 0,
 	"iron" = 0,
 	"wood" = 0,
 	"gold" = 0
+} :
+	get:
+		return resources
+	set(value):
+		resources = value
+
+const states : Dictionary = {
+	"standing" : "standing",
+	"walking" : "walking",
+	"gathering" : "gathering",
+	"editing" : "editing"
 }
-var editable : bool = false
+var state : String = states.standing :
+	get:
+		return state
+	set(value):
+		state = value
+		match state:
+			states.editing:
+				emit_signal("editing_mode_entered")
 
 
 @export var max_move_speed : float = 5.0
@@ -25,6 +45,7 @@ var velocity : Vector3 = Vector3.ZERO
 var dish_rotation : Vector2 = Vector2.ZERO
 const max_rots = .5
 @onready var init_body_rots : Quaternion
+@onready var init_body_pos : Vector3
 
 @onready var skeleton = $Armature/Skeleton3D
 @onready var collider = $Armature/Skeleton3D/Collisions/Collider
@@ -38,12 +59,25 @@ func _____OVERRIDES(): pass
 func _ready():
 	var body = skeleton.find_bone("Body.U")
 	init_body_rots = skeleton.get_bone_pose_rotation(body)
+	init_body_pos = skeleton.get_bone_pose_position(body)
 
 
 func _physics_process(delta):
 	
 	handle_movement(delta)
-	editable = velocity.length() >= .01
+	
+	if Input.is_action_just_pressed("mv_edit_mode"):
+		match state:
+			states.standing:
+				state = states.editing
+			states.editing:
+				state = states.standing
+	elif Input.is_action_just_pressed("mv_action"):
+		match state:
+			states.standing:
+				state = states.gathering
+			states.gathering:
+				state = states.standing
 
 
 
@@ -57,31 +91,63 @@ func handle_movement(delta):
 	var dir_h : float = Input.get_axis("mv_left", "mv_right")
 	var dir : Vector2 = Vector2(dir_h, dir_v).normalized()
 	
-	move_speed = clampf(move_speed + acceleration * (1 if dir_h || dir_v else -1), 0, max_move_speed)
-	
-	velocity = lerp(velocity, Vector3(dir.x, 0.0, dir.y) * move_speed * delta, acceleration)
-	
-	# Apply position
-	translate(velocity)
-	
-	# ----Handle rotations
-	dish_rotation = Vector2(lookat_back.position.y, lookat_right.position.y)
-	dish_rotation += \
-		(Vector2(dir_v, dir_h) - dish_rotation) * .05
-	lookat_back.position.y = clampf(dish_rotation.x, -max_rots, max_rots)
-	lookat_right.position.y = clampf(dish_rotation.y, -max_rots, max_rots)
-	# Calculate rotations
 	var body = skeleton.find_bone("Body.R")
+	var _lowered : Vector3 = init_body_pos + Vector3(0, -1, 0)
 	
-	tracker_back.look_at(lookat_back.global_position, Vector3.UP)
-	tracker_right.look_at(lookat_right.global_position, Vector3.UP)
-	
-	var new_rots = (Quaternion.from_euler(tracker_back.rotation) * \
-		Quaternion.from_euler(tracker_right.rotation)).normalized()
-	
-	# Apply rotation
-	skeleton.set_bone_pose_rotation(body, new_rots)
-	#collider.rotation = -new_rots.get_euler(2)
+	if state in [states.walking, states.standing]:
+		move_speed = clampf(
+			move_speed + acceleration * (1 if dir_h || dir_v else -1),
+			0,
+			max_move_speed
+		)
+		
+		velocity = lerp(
+			velocity,
+			Vector3(dir.x, 0.0, dir.y) * move_speed * delta,
+			acceleration
+		)
+		
+		if velocity.length() >= 0.01:
+			state = states.walking
+		else:
+			state = states.standing
+		
+		# Apply position
+		translate(velocity)
+		
+		# ----Handle rotations
+		dish_rotation = Vector2(lookat_back.position.y, lookat_right.position.y)
+		dish_rotation += \
+			(Vector2(dir_v, dir_h) - dish_rotation) * .05
+		lookat_back.position.y = clampf(dish_rotation.x, -max_rots, max_rots)
+		lookat_right.position.y = clampf(dish_rotation.y, -max_rots, max_rots)
+		# Calculate rotations
+		
+		tracker_back.look_at(lookat_back.global_position, Vector3.UP)
+		tracker_right.look_at(lookat_right.global_position, Vector3.UP)
+		
+		var new_rots = (Quaternion.from_euler(tracker_back.rotation) * \
+			Quaternion.from_euler(tracker_right.rotation)).normalized()
+		
+		# Apply rotation
+		skeleton.set_bone_pose_rotation(body, new_rots)
+		
+		# -- Cancel body lowering when no longer gathering resource
+		var _new_body_pos = skeleton.get_bone_pose_position(body)
+		#_new_body_pos += (init_body_pos - _new_body_pos) * .05
+		_new_body_pos += Vector3(0, .01, 0)
+		_new_body_pos = _new_body_pos.clamp(_lowered, init_body_pos)
+		skeleton.set_bone_pose_position(body, _new_body_pos)
+	elif state == states.gathering:
+		# -- Handle body lowering when gathering resource
+		var _new_body_pos = skeleton.get_bone_pose_position(body)
+		#_new_body_pos += ((init_body_pos - Vector3(0, 1, 0)) - _new_body_pos) * .05
+		_new_body_pos -= Vector3(0, .01, 0)
+		_new_body_pos = _new_body_pos.clamp(_lowered, init_body_pos)
+		skeleton.set_bone_pose_position(body, _new_body_pos)
+
+func exit_editing_mode():
+	state = states.standing
 
 
 
